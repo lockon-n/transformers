6e5e9e0a590516b321e756839014448e50952083
NielsRogge <niels.rogge1@gmail.com>
2022-03-12 16:55:29 +0000

    Move tests to dedicated folder

--- utils/check_repo.py
+++ utils/check_repo.py
@@ -124,6 +124,7 @@ IGNORE_NON_AUTO_CONFIGURED = PRIVATE_MODELS.copy() + [
     # models to ignore for model xxx mapping
     "DecisionTransformerGPT2Model",
     "GLPNForDepthEstimation",
+    "ViltForQuestionAnswering",
     "ViltForImagesAndTextClassification",
     "ViltForMaskedLM",
     "XGLMEncoder",
