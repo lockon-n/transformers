98de7822e21d5c8d90bdd91281824e9ab27d5148
NielsRogge <niels.rogge1@gmail.com>
2022-05-04 07:10:55 +0000

    Improve docs, move tests to model folder

--- utils/check_repo.py
+++ utils/check_repo.py
@@ -122,10 +122,12 @@ TEST_FILES_WITH_NO_COMMON_TESTS = [
 # should **not** be the rule.
 IGNORE_NON_AUTO_CONFIGURED = PRIVATE_MODELS.copy() + [
     # models to ignore for model xxx mapping
+    "DPTForDepthEstimation",
     "DecisionTransformerGPT2Model",
     "GLPNForDepthEstimation",
     "ViltForQuestionAnswering",
     "ViltForImagesAndTextClassification",
+    "ViltForImageAndTextRetrieval",
     "ViltForMaskedLM",
     "XGLMEncoder",
     "XGLMDecoder",
